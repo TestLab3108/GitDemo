e480aadf4917a4c0c6af498a0714c1da9d51a489
TestLab3108 <Sufiyan.TestLab@gmail.com>
2024-06-08 19:14:02 +0530

    There is an intails commit in Test Login Page

--- src/GMTestPackage/TestScriptGM.java
+++ src/GMTestPackage/TestScriptGM.java
@@ -8,9 +8,11 @@ public class TestScriptGM {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+//		String Key="webdriver.chrome.driver";
+//		String Value="./chromedriver.exe";
+		System.setProperty(Key, Value);
 		String Key="webdriver.chrome.driver";
 		String Value="./chromedriver.exe";
-		System.setProperty(Key, Value);
 		
 		WebDriver driver=new ChromeDriver();
 	  
