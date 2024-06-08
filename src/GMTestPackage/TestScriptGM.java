a208e7b7f276bf502f4e4bdc78f323064846097f
TestLab3108 <Sufiyan. TestLab@gmail.com>
2024-06-08 13:06:48 +0530

    First Commit

---
+++ src/GMTestPackage/TestScriptGM.java
@@ -0,0 +1,24 @@
+package GMTestPackage;
+
+import Practise.ChromeDriver;
+import Practise.WebDriver;
+
+public class TestScriptGM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String Key="webdriver.chrome.driver";
+		String Value="./chromedriver.exe";
+		System.setProperty(Key, Value);
+		
+		WebDriver driver=new ChromeDriver();
+	  
+	  
+	  driver.get("https://gurumavin.in/");
+	  driver.get("https://gurumavin.in/student-account");
+	   //Thread.sleep(10000);
+
+	}
+
+}
---
+++ src/GMTestPackage/WebDriver.java
@@ -0,0 +1,5 @@
+package GMTestPackage;
+
+public class WebDriver {
+
+}
